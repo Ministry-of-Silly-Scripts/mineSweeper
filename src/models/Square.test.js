426302ee2e63ef421126c1472c071986e3470b34
Marcio Silva <marcio.silva@ibm.com>
2023-12-10 20:39:27 -0300

    test: add square initialization test (bomb)
    - it should not be a bomb

--- src/models/Square.test.js
+++ src/models/Square.test.js
@@ -5,4 +5,9 @@ describe('Square', () => {
     const square = new Square();
     expect(square.isRevealed).toBe(false);
   });
+
+  it('should not be a bomb when initialized', () => {
+    const square = new Square();
+    expect(square.isBomb).toBe(false);
+  });
 });
